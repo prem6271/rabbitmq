b5bdb12a6410c23d9da8ca67048ab2b1b398025b
Rohini <rohini@vknowlabs.com>
2019-11-11 18:44:26 +0530

    Initial commit

---
+++ worker.js
@@ -0,0 +1,23 @@
+var amqp = require('amqplib');
+
+amqp.connect('amqp://localhost').then(function(conn) {
+  process.once('SIGINT', function() { conn.close(); });
+  return conn.createChannel().then(function(ch) {
+    var ok = ch.assertQueue('db_connection_queue1', {durable: true});
+    ok = ok.then(function() { ch.prefetch(1); });
+    ok = ok.then(function() {
+      ch.consume('db_connection_queue1', doWork, {noAck: false});
+    });
+    return ok;
+
+    function doWork(msg) {
+      var body = msg.content.toString();
+      console.log(" [x] Received %s", body );
+      var secs = body.split('.').length - 1;
+      setTimeout(function() {
+        console.log(" [x] Done");
+        ch.ack(msg);
+      }, secs * 1000);
+    }
+  });
+}).catch(console.warn);
