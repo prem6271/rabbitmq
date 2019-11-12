815a0bfe9f046bb60cfda9fc85bc1c8150e1eda9
Rohini <rohini@vknowlabs.com>
2019-11-12 15:57:54 +0530

    Execute mongo query

--- task.js
+++ task.js
@@ -2,13 +2,12 @@ var amqp = require('amqplib');
 
 amqp.connect('amqp://localhost').then(function(conn) {
   return conn.createChannel().then(function(ch) {
-    var q = 'db_connection_queue1';
+    var q = 'db_connection_queue';
     var ok = ch.assertQueue(q, {durable: true});
     return ok.then(function() {
       var msg = process.argv.slice(2)[0] ; 
-      
-      ch.sendToQueue(q, Buffer.from( JSON.stringify(msg) ), {deliveryMode: true});
-      console.log(" [x] Sent '%s'", msg  );
+      ch.sendToQueue(q, Buffer.from( JSON.stringify(JSON.parse(msg)) ), {deliveryMode: true});
+      console.log(" [x] Sent '%s'", msg   );
       return ch.close();
     });
   })
